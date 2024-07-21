2ea99c5b059e54ab5dac0b46ca48d63accda030c
Juan-Vera-ok <juanvera2001@hotmail.com>
2024-07-21 08:16:42 -0300

    First commit

---
+++ src/Icon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export default function Icon() {
+    return (
+        <div style={{margin: '-75px 10px'}}>
+            <img className='App-logo' width="100%" style={{objectFit: 'contain'}} src="logo512.png" alt="logo"/>
+        </div>
+    )
+}
